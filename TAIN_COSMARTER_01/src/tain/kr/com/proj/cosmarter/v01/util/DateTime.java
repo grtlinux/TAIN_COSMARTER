a01e531524c207094fe962e3a0a9cbc64773e8d1
Kiea Seok Kang <grtlinux@gmail.com>
2016-04-14 14:32:29 +0900

    .

--- TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/util/DateTime.java
+++ TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/util/DateTime.java
@@ -22,6 +22,7 @@ package tain.kr.com.proj.cosmarter.v01.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 
@@ -46,10 +47,23 @@ public class DateTime {
 	private static final Logger log = Logger.getLogger(DateTime.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final String KEY_FORMAT_YYYYMMDD = "tain.datetime.format.yyyymmdd";
+	private static final String KEY_FORMAT_YYYYMMDDHHMMSS = "tain.datetime.format.yyyymmddhhmmss";
+	
+	private String strFormatYYYYMMDD = null;
+	private String strFormatYYYYMMDDHHMMSS = null;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	private DateTime() throws Exception {
 		if (flag) {
+			String clsName = this.getClass().getName();
 			
+			ResourceBundle rb = ResourceBundle.getBundle(clsName.replace('.', '/'));
+			
+			this.strFormatYYYYMMDD = rb.getString(KEY_FORMAT_YYYYMMDD);
+			this.strFormatYYYYMMDDHHMMSS = rb.getString(KEY_FORMAT_YYYYMMDDHHMMSS);
 		}
 	}
 	
@@ -57,7 +71,7 @@ public class DateTime {
 		String ret = null;
 		
 		if (flag) {
-			ret = new SimpleDateFormat("yyyyMMdd", Locale.KOREA).format(new Date());
+			ret = new SimpleDateFormat(this.strFormatYYYYMMDD, Locale.KOREA).format(new Date());
 		}
 		
 		return ret;
@@ -67,7 +81,7 @@ public class DateTime {
 		String ret = null;
 		
 		if (flag) {
-			ret = new SimpleDateFormat("yyyyMMddHHmmss", Locale.KOREA).format(new Date());
+			ret = new SimpleDateFormat(this.strFormatYYYYMMDDHHMMSS, Locale.KOREA).format(new Date());
 		}
 		
 		return ret;
