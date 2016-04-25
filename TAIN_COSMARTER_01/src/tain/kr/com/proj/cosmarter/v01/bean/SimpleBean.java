a88afce4907e396f599219a4f6bb242637d837ee
Kiea Seok Kang <grtlinux@gmail.com>
2016-04-25 16:46:36 +0900

    .

--- TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/bean/SimpleBean.java
+++ TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/bean/SimpleBean.java
@@ -19,6 +19,10 @@
  */
 package tain.kr.com.proj.cosmarter.v01.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -35,12 +39,210 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
+
 public class SimpleBean {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(SimpleBean.class);
 
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String KEY_IPADDR  = "tain.cosmarter.bean.simple.ipaddr";
+	private static final String KEY_PORT    = "tain.cosmarter.bean.simple.port";
+	private static final String KEY_NAME    = "tain.cosmarter.bean.simple.name";
+	private static final String KEY_CMD     = "tain.cosmarter.bean.simple.cmd";
+	private static final String KEY_RETINFO = "tain.cosmarter.bean.simple.ret.info";
+
+	private static final String KEY_SKIP    = "tain.cosmarter.bean.simple.skip.lines";
+	
+	private String ipAddr = null;
+	private String port = null;
+	
+	private String name = null;
+	private String cmd = null;
+	private String retInfo = null;
+	private String skip = null;
+	
+	private List<SimpleInfo> lstInfo = null;
+	
+	private String result = null;
+	
+	private class SimpleInfo {
+		
+		public String name;
+		public boolean usable;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public SimpleBean() throws Exception {
+		
+		if (flag) {
+			String clsName = this.getClass().getName();
+			
+			ResourceBundle rb = ResourceBundle.getBundle(clsName.replace('.', '/'));
+			
+			this.ipAddr = rb.getString(KEY_IPADDR);
+			this.port = rb.getString(KEY_PORT);
+			this.name = rb.getString(KEY_NAME);
+			this.cmd = rb.getString(KEY_CMD);
+			this.retInfo = rb.getString(KEY_RETINFO);
+			this.skip = rb.getString(KEY_SKIP);
+			
+			if (!flag) print();
+		}
+		
+		if (flag) {
+			lstInfo = new ArrayList<SimpleInfo>();
+			
+			String[] infos = this.retInfo.split(",");
+			
+			for (String info : infos) {
+				String[] items = info.split(":");
+				if (items.length != 2)
+					continue;
+				
+				SimpleInfo simpleInfo = new SimpleInfo();
+				simpleInfo.name = items[0].trim();
+				simpleInfo.usable = "1".equals(items[1].trim()) ? true : false;
+				
+				lstInfo.add(simpleInfo);
+			}
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * @return the ipAddr
+	 */
+	public String getIpAddr() {
+		return ipAddr;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public String getPort() {
+		return port;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the cmd
+	 */
+	public String getCmd() {
+		return cmd;
+	}
+
+	/**
+	 * @return the retInfo
+	 */
+	public String getRetInfo() {
+		return retInfo;
+	}
+
+	/**
+	 * @param ipAddr the ipAddr to set
+	 */
+	public void setIpAddr(String ipAddr) {
+		this.ipAddr = ipAddr;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param cmd the cmd to set
+	 */
+	public void setCmd(String cmd) {
+		this.cmd = cmd;
+	}
+
+	/**
+	 * @param retInfo the retInfo to set
+	 */
+	public void setRetInfo(String retInfo) {
+		this.retInfo = retInfo;
+	}
+
+	/**
+	 * @return the skip
+	 */
+	public String getSkip() {
+		return skip;
+	}
+
+	/**
+	 * @param skip the skip to set
+	 */
+	public void setSkip(String skip) {
+		this.skip = skip;
+	}
+
+	/**
+	 * @return the result
+	 */
+	public String getResult() {
+		return result;
+	}
+
+	/**
+	 * @param result the result to set
+	 */
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public String getInfoName(int idx) throws Exception {
+		
+		SimpleInfo info = lstInfo.get(idx);
+		
+		if (info.usable)
+			return info.name;
+		
+		return null;
+	}
+
+	public int getInfoCnt() throws Exception {
+		return lstInfo.size();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void print() throws Exception {
+		
+		if (flag) {
+
+			log.debug(" ipAddr  = " + this.getIpAddr());
+			log.debug(" port    = " + this.getPort());
+			log.debug(" name    = " + this.getName());
+			log.debug(" cmd     = " + this.getCmd());
+			log.debug(" retInfo = " + this.getRetInfo());
+			log.debug(" skip    = " + this.getSkip());
+			
+			log.debug(" result  = " + this.getResult());
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -49,6 +251,9 @@ public class SimpleBean {
 		
 		if (flag) {
 			
+			SimpleBean bean = new SimpleBean();
+			
+			bean.print();
 		}
 	}
 	
--- TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/main/client/CoBeanClient.java
+++ TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v01/main/client/CoBeanClient.java
@@ -19,7 +19,16 @@
  */
 package tain.kr.com.proj.cosmarter.v01.main.client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ResourceBundle;
+
 import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.cosmarter.v01.bean.SimpleBean;
+import tain.kr.com.proj.cosmarter.v01.util.CheckSystem;
 
 /**
  * Code Templates > Comments > Types
@@ -42,13 +51,145 @@ public class CoBeanClient {
 	private static final Logger log = Logger.getLogger(CoBeanClient.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String KEY_DESC = "tain.cobean.client.desc";
+	
+	private String strDesc = null;
+	
+	public CoBeanClient() throws Exception {
+		
+		if (flag) {
+			String clsName = this.getClass().getName();
+			
+			ResourceBundle rb = ResourceBundle.getBundle(clsName.replace('.', '/'));
+			
+			this.strDesc = rb.getString(KEY_DESC);
+			
+			if (flag) this.print();
+		}
+	}
+	
+	public String getDesc() throws Exception {
+		
+		return this.strDesc;
+	}
+	
+	public void print() throws Exception {
+		
+		if (flag) {
+			log.debug(">>>>> desc = " + this.getDesc());
+		}
+	}
+	
+	public void process(SimpleBean bean) throws Exception {
+		
+		if (flag) {
+			PrintWriter pw = null;
+			BufferedReader br = null;
+			String line = null;
+			
+			Socket socket = new Socket(bean.getIpAddr(), Integer.parseInt(bean.getPort()));
+			
+			pw = new PrintWriter(socket.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			if (flag) log.info(">>>>> CMD : " + bean.getCmd());
+			pw.println(bean.getCmd());
+			pw.flush();
+			
+			int noSkip = Integer.parseInt(bean.getSkip());
+			int noLine = 0;
+			int limit = bean.getInfoCnt();
+			
+			StringBuffer sbJson = new StringBuffer();
+
+			sbJson.append("{").append("\"").append(bean.getName()).append("\"").append(":").append("[");
+			if (flag) sbJson.append(CheckSystem.getInstance().getLineSeparator());
+
+			while ((line = br.readLine()) != null) {
+				++noLine;
+				if (noLine <= noSkip)
+					continue;
+				
+				if (!flag) log.debug(String.format(">>>>> (%04d) [%s]", noLine, line));
+				
+				String[] words = line.split("\\s+", limit);
+				if (flag) words[limit-1] = words[limit-1].trim();
+				
+				// make JSON
+				StringBuffer sb = new StringBuffer();
+				if (flag) sb.append("\t");
+				sb.append("{");
+				
+				for (int i=0; i < limit; i++) {
+					String infoName = bean.getInfoName(i);
+					if (infoName == null)
+						continue;
+					
+					sb.append("\"").append(infoName).append("\"");
+					sb.append(":");
+					sb.append("\"").append(words[i]).append("\"");
+					sb.append(",");
+				}
+				
+				sb.append("},");
+				if (flag) sb.append(CheckSystem.getInstance().getLineSeparator());
+				
+				if (flag) sbJson.append(sb.toString());
+			}
+			
+			sbJson.append("]}");
+			if (flag) sbJson.append(CheckSystem.getInstance().getLineSeparator());
+
+			bean.setResult(sbJson.toString());
+			
+			br.close();
+			pw.close();
+			socket.close();
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static CoBeanClient instance = null;
+	
+	public static synchronized CoBeanClient getInstance() throws Exception {
+		
+		if (instance == null) {
+			instance = new CoBeanClient();
+		}
+		
+		return instance;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	private static void test01(String[] args) throws Exception {
 		
 		if (flag) {
+			CoBeanClient.getInstance();
+		}
+	}
+	
+	private static void test02(String[] args) throws Exception {
+		
+		SimpleBean bean = new SimpleBean();
+
+		if (flag) {
+			bean.setSkip("0");
+			CoBeanClient.getInstance().process(bean);
 			
+			if (flag) log.debug(">>>>> result = " + bean.getResult());
+		}
+		
+		if (flag) {
+			bean.setName("lstPs2");
+			bean.setCmd("ps -ef | grep kang | grep -v grep");
+			CoBeanClient.getInstance().process(bean);
+			
+			if (flag) log.debug(">>>>> result = " + bean.getResult());
 		}
 	}
 	
@@ -56,6 +197,7 @@ public class CoBeanClient {
 		
 		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
 		
-		if (flag) test01(args);
+		if (!flag) test01(args);
+		if (flag) test02(args);
 	}
 }
