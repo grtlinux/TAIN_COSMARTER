29dedda9464477f6d3afb788cc743ab127bc1b10
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-11 01:47:43 +0900

    .

---
+++ TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v02/common/version/Version.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.cosmarter.v02.common.version;
+
+import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Version.java
+ *   -. Package    : tain.kr.com.proj.cosmarter.v01.common
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 4. 11. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Version {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Version.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final String KEY_VERSION_INFO = "tain.cosmarter.version.info";
+	private static final String KEY_VERSION_DESC = "tain.cosmarter.version.desc";
+	
+	private String strVersionInfo = null;
+	private String strVersionDesc = null;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private Version() throws Exception {
+		
+		if (flag) {
+			String clsName = this.getClass().getName();
+			
+			ResourceBundle rb = ResourceBundle.getBundle(clsName.replace('.', '/'));
+			
+			this.strVersionInfo = rb.getString(KEY_VERSION_INFO);
+			this.strVersionDesc = rb.getString(KEY_VERSION_DESC);
+		}
+	}
+	
+	public String getInfo() throws Exception {
+		return this.strVersionInfo;
+	}
+	
+	public String getDesc() throws Exception {
+		return this.strVersionDesc;
+	}
+	
+	public void print() throws Exception {
+		System.out.println("INFO > " + this.getInfo());
+		System.out.println("DESC > " + this.getDesc());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static Version instance = null;
+	
+	public static synchronized Version getInstance() throws Exception {
+		
+		if (instance == null) {
+			instance = new Version();
+		}
+		
+		return instance;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static void test01(String[] args) throws Exception {
+		
+		if (flag) {
+			Version.getInstance().print();
+			
+			log.debug(">>>>> " + Version.getInstance().getInfo());
+			log.debug(">>>>> " + Version.getInstance().getDesc());
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
+}
