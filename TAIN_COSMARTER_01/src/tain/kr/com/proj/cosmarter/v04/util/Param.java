a137931146a2d81d3edd4953e1d670459f39ac13
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-17 04:41:33 +0900

    .

--- TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v04/util/Param.java
+++ TAIN_COSMARTER_01/src/tain/kr/com/proj/cosmarter/v04/util/Param.java
@@ -19,6 +19,11 @@
  */
 package tain.kr.com.proj.cosmarter.v04.util;
 
+import java.util.Enumeration;
+import java.util.MissingResourceException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -35,24 +40,103 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class Param {
+public final class Param {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(Param.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	protected static final String FILE_NAME_RESOURCES = "resources/resources";
+
+	protected final Properties prop;
+	protected final ResourceBundle resourceBundle;
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
 	public Param() {
+
+		this.prop = System.getProperties();
+		this.resourceBundle = ResourceBundle.getBundle(FILE_NAME_RESOURCES);
+
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private String getStringFromSystem(String key) {
+		return this.prop.getProperty(key);
+	}
+	
+	private String getStringFromResourceBundle(String key) {
+		
+		String strValue = null;
+		
+		try {
+			strValue = this.resourceBundle.getString(key);
+		} catch (MissingResourceException e) {}
+		
+		return strValue;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public String getString(String key) {
+		
+		String value;
+		
+		value = getStringFromSystem(key);
+		if (value != null)
+			return value;
+		
+		value = getStringFromResourceBundle(key);
+		
+		return value;
+	}
+	
+	public String getString(String key, String defaultValue) {
+		
+		String value = getString(key);
+		if (value == null)
+			return defaultValue;
+		
+		return value;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void printAll() {
+		
+		if (flag) {
+			/*
+			 * System properties
+			 */
+			if (flag) System.out.println("########### System properties ##########");
+			
+			this.prop.list(System.out);
+		}
+		
+		if (flag) {
+			/*
+			 * Resource properties
+			 */
+			if (flag) System.out.println("########### resources/resources.properties ##########");
+
+			Enumeration<String> enumKeys = this.resourceBundle.getKeys();
+			while (enumKeys.hasMoreElements()) {
+				String strKey = (String) enumKeys.nextElement();
+				String strVal = (String) this.resourceBundle.getString(strKey);
+				
+				System.out.printf("resources [%s] = [%s]\n", strKey, strVal);
+			}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -62,6 +146,17 @@ public class Param {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static Param instance = null;
+	
+	public static synchronized Param getInstance() throws Exception {
+		
+		if (Param.instance == null)
+			Param.instance = new Param();
+		
+		return Param.instance;
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -70,11 +165,21 @@ public class Param {
 	 */
 	private static void test01(String[] args) throws Exception {
 
-		if (flag)
-			new Param();
-
 		if (flag) {
-
+			/*
+			 * System.getProperties
+			 * resources/resources.properties
+			 */
+			String strKey = "tain.project";
+			
+			if (flag) System.out.printf("Param : [%s] = [%s]\n", strKey, Param.getInstance().getString(strKey));
+		}
+		
+		if (flag) {
+			/*
+			 * printAll
+			 */
+			Param.getInstance().printAll();
 		}
 	}
 
